e03bb4c2b84d920ef6a786365f01d420de0f5f86
Bruno N Fernandes <bruno.tere@gmail.com>
2019-03-30 00:45:06 -0300

    Inclusão das Classes de Criptografia e de Assertivas

--- src/Utils/Abstracao/Entidade.cs
+++ src/Utils/Abstracao/Entidade.cs
@@ -13,11 +13,38 @@ namespace MPSC.PlenoSoft.Core.Utils.Abstracao
 		TId Id { get; set; }
 	}
 
-	public abstract class Entidade : IUniqueId, IEntidade<Int64>
-	{
-		[Display(Name = "Id")]
-		public virtual Int64 Id { get; set; }
 
+	public abstract class ClasseBase<TId> where TId : struct, IComparable, IFormattable, IConvertible, IComparable<TId>, IEquatable<TId>
+	{
+		protected static Int64 _controle = 0;
+
+		[Display(Name = "Id")]
+		public TId Id { get; set; }
+
+		public virtual void EhValido() { }
+	}
+
+	public abstract class Tipo : ClasseBase<Int16>
+	{
+		public String Sigla { get; set; }
+		public String Descricao { get; set; }
+		public Int16 OrdemApresentacao { get; set; }
+	}
+
+	public abstract class Cadastro : ClasseBase<Int32>
+	{
+		public DateTime Inclusao { get; set; }
+		public DateTime? Alteracao { get; set; }
+		public String LoginInclusao { get; set; }
+		public String LoginAlteracao { get; set; }
+	}
+
+	public abstract class Entidade : ClasseBase<Int64>, IUniqueId, IEntidade<Int64>
+	{
 		Int64 IUniqueId.UId { get => Id; set => Id = value; }
+		public DateTime Inclusao { get; set; }
+		public DateTime? Alteracao { get; set; }
+		public String LoginInclusao { get; set; }
+		public String LoginAlteracao { get; set; }
 	}
 }
---
+++ src/Utils/Statics/Criptografia.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MPSC.PlenoSoft.Core.Utils.Statics
+{
+	public static class Criptografia
+	{
+		private static readonly Encoding _encoding = Encoding.UTF8;
+		private const String cKey = "p2eS9hDBNZCxmyOdeGyDgQ==";
+		private const String cIV = "IVJjFZBCW9w=";
+
+		public static String Encripta(this String mensagem)
+		{
+			return Encripta(mensagem, cKey, cIV);
+		}
+
+		public static String Decripta(this String mensagem)
+		{
+			return Decripta(mensagem, cKey, cIV);
+		}
+
+		public static String Criptografar(this String mensagem)
+		{
+			var stringBuilder = new StringBuilder();
+			var md5CryptoServiceProvider = new MD5CryptoServiceProvider();
+			var bytes = md5CryptoServiceProvider.ComputeHash(_encoding.GetBytes(mensagem));
+
+			foreach (var vByte in bytes)
+				stringBuilder.Append(vByte.ToString("x2"));
+
+			return stringBuilder.ToString().ToLower();
+		}
+
+		public static String Encripta(this String mensagem, String key, String vetor)
+		{
+			var bytes = _encoding.GetBytes(mensagem);
+			var rc2CryptoServiceProvider = new RC2CryptoServiceProvider();
+			var iCryptoTransform = rc2CryptoServiceProvider.CreateEncryptor(Convert.FromBase64String(key), Convert.FromBase64String(vetor));
+			var memoryStream = new MemoryStream();
+
+			var cryptoStream = new CryptoStream(memoryStream, iCryptoTransform, CryptoStreamMode.Write);
+			cryptoStream.Write(bytes, 0, bytes.Length);
+			cryptoStream.FlushFinalBlock();
+			cryptoStream.Close();
+
+			return Convert.ToBase64String(memoryStream.ToArray());
+		}
+
+		public static String Decripta(this String mensagem, String key, String vetor)
+		{
+			var buffer = new Byte[256];
+			var stringBuilder = new StringBuilder();
+			var rc2CryptoServiceProvider = new RC2CryptoServiceProvider();
+			var iCryptoTransform = rc2CryptoServiceProvider.CreateDecryptor(Convert.FromBase64String(key), Convert.FromBase64String(vetor));
+			var memoryStream = new MemoryStream(Convert.FromBase64String(mensagem));
+
+			var cryptoStream = new CryptoStream(memoryStream, iCryptoTransform, CryptoStreamMode.Read);
+			var bytesLidos = cryptoStream.Read(buffer, 0, buffer.Length);
+			while (bytesLidos == buffer.Length)
+			{
+				stringBuilder.Append(_encoding.GetString(buffer, 0, bytesLidos));
+				bytesLidos = cryptoStream.Read(buffer, 0, buffer.Length);
+			}
+			cryptoStream.Close();
+
+			return stringBuilder.ToString();
+		}
+	}
+}

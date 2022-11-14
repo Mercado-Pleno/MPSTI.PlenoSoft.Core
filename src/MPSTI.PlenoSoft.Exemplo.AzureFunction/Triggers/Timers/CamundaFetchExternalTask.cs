c42420f4e4979807aad86e57838db57f8aa1da8e
Bruno N Fernandes <bruno@mercadopleno.com.br>
2022-11-14 01:13:01 -0300

    ajuste no CamundaFetchExternalTask

--- src/MPSTI.PlenoSoft.Exemplo.AzureFunction/Triggers/Timers/CamundaFetchExternalTask.cs
+++ src/MPSTI.PlenoSoft.Exemplo.AzureFunction/Triggers/Timers/CamundaFetchExternalTask.cs
@@ -13,34 +13,42 @@ namespace MPSTI.PlenoSoft.Exemplo.AzureFunction.Triggers.Timers
 		private const string Timer1 = "%Camunda_TimerTrigger_Dia%";
 		private const string Timer2 = "%Camunda_TimerTrigger_Noi%";
 		private const string Timer3 = "%Camunda_TimerTrigger_Mad%";
+		private ILogger _logger;
 
 		public CamundaFetchExternalTask(ICamundaClient camundaClient) : base(camundaClient) { }
 
 
 		[FunctionName("FetchExternalTask_Timer1")]
-		public async Task FetchExternalTask_Timer1([TimerTrigger(Timer1)] TimerInfo t, ILogger l)
-			=> await FetchExternalTaskOnTimer(t, l);
+		public async Task FetchExternalTask_Timer1([TimerTrigger(Timer1)] TimerInfo timerInfo, ILogger logger)
+			=> await FetchExternalTaskOnTimer(timerInfo.Schedule.ToString(), logger);
 
 
 		[FunctionName("FetchExternalTask_Timer2")]
-		public async Task FetchExternalTask_Timer2([TimerTrigger(Timer2)] TimerInfo t, ILogger l)
-			=> await FetchExternalTaskOnTimer(t, l);
+		public async Task FetchExternalTask_Timer2([TimerTrigger(Timer2)] TimerInfo timerInfo, ILogger logger)
+			=> await FetchExternalTaskOnTimer(timerInfo.Schedule.ToString(), logger);
 
 
 		[FunctionName("FetchExternalTask_Timer3")]
-		public async Task FetchExternalTask_Timer3([TimerTrigger(Timer3)] TimerInfo t, ILogger l)
-			=> await FetchExternalTaskOnTimer(t, l);
+		public async Task FetchExternalTask_Timer3([TimerTrigger(Timer3)] TimerInfo timerInfo, ILogger logger)
+			=> await FetchExternalTaskOnTimer(timerInfo.Schedule.ToString(), logger);
 
 
-		private async Task FetchExternalTaskOnTimer(TimerInfo timer, ILogger log)
+		private async Task FetchExternalTaskOnTimer(string timerSchedule, ILogger logger)
 		{
-			log.LogInformation($"{timer.Schedule}: {DateTime.Now}");
-			await Task.CompletedTask;
+			_logger = logger;
+			await ExternalTaskFetchAndLock($"FetchExternalTaskOnTimer::{timerSchedule}");
 		}
 
 		protected override async Task EnqueueExternalTask(ExternalTask externalTask)
-		{
-			await Task.CompletedTask;
-		}
+			=> await Task.CompletedTask;
+
+		protected override void LogInformation(string message, params object[] args)
+			=> _logger.LogInformation(message, args);
+
+		protected override void LogTrace(string message, params object[] args)
+			=> _logger.LogTrace(message, args);
+
+		protected override void LogError(Exception exception, string message, params object[] args)
+			=> _logger.LogError(exception, message, args);
 	}
 }

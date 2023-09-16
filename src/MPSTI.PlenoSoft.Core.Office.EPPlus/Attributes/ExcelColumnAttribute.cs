e7c5ac90fbffcdb27bc1f3b9d7f71d94d2446fa0
Bruno N Fernandes <bruno@mercadopleno.com.br>
2023-09-16 18:05:35 -0300

    2023/09/16 - 1.0.0.19 -> Correção para respeitar a posição de cada coluna no excel;

--- src/MPSTI.PlenoSoft.Core.Office.EPPlus/Attributes/ExcelColumnAttribute.cs
+++ src/MPSTI.PlenoSoft.Core.Office.EPPlus/Attributes/ExcelColumnAttribute.cs
@@ -3,55 +3,65 @@ using System.Reflection;
 
 namespace MPSTI.PlenoSoft.Core.Office.EPPlus.Attributes
 {
-	public enum TipoEnum
-	{
-		String,
-		Int,
-		Long,
-		Double,
-		Decimal,
-		DateTime,
-		ExcelGeneral
-	}
+    public enum TipoEnum
+    {
+        General,
+        String,
+        Int,
+        Long,
+        Double,
+        Decimal,
+        DateTime,
+    }
 
-	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-	public class ExcelColumnAttribute : Attribute
-	{
-		public PropertyInfo PropertyInfo { get; private set; }
-		public double Largura { get; set; }
-		public string PropertyName { get; set; }
-		public string Titulo { get; set; }
-		public int Posicao { get; set; }
-		public string Formato { get; set; }
-		public TipoEnum Tipo { get; set; }
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class ExcelColumnAttribute : Attribute
+    {
+        public PropertyInfo PropertyInfo { get; private set; }
+        public double Largura { get; set; }
+        public string PropertyName { get; set; }
+        public string Titulo { get; set; }
+        public int Posicao { get; set; }
+        public string Formato { get; set; }
+        public TipoEnum Tipo { get; set; }
 
-		public string NumberFormat { get; set; }
+        public string NumberFormat { get; set; }
 
-		public string DataFormat
-		{
-			get { return Formato ?? (Formato = "{0:yyyy/MM/dd}"); }
-			set { Formato = value; }
-		}
+        public string DataFormat
+        {
+            get { return Formato ?? (Formato = "{0:yyyy/MM/dd}"); }
+            set { Formato = value; }
+        }
 
-		public ExcelColumnAttribute(string titulo, int posicao) : this(null, titulo, posicao, null, null) { }
+        public ExcelColumnAttribute(string titulo, int posicao) : this(null, titulo, posicao, null, null) { }
 
-		public ExcelColumnAttribute(string propertyName, string titulo, int posicao) : this(propertyName, titulo, posicao, null, null) { }
+        public ExcelColumnAttribute(string propertyName, string titulo, int posicao, TipoEnum? tipo = null, string formato = null)
+        {
+            PropertyName = propertyName;
+            Largura = 20.00;
+            Titulo = titulo;
+            Posicao = posicao;
+            Tipo = tipo ?? TipoEnum.String;
+            if (formato != null) Formato = formato;
+        }
 
-		public ExcelColumnAttribute(string propertyName, string titulo, int posicao, TipoEnum? tipo = null, string formato = null)
-		{
-			PropertyName = propertyName;
-			Largura = 20.00;
-			Titulo = titulo;
-			Posicao = posicao;
-			Tipo = tipo ?? TipoEnum.String;
-			if (formato != null) Formato = formato;
-		}
+        public ExcelColumnAttribute Mapear(PropertyInfo propertyInfo)
+        {
+            PropertyName = propertyInfo.DeclaringType.Name + "." + propertyInfo.Name;
+            PropertyInfo = propertyInfo;
+            return this;
+        }
 
-		public ExcelColumnAttribute Mapear(PropertyInfo propertyInfo)
-		{
-			PropertyName = propertyInfo.DeclaringType.Name + "." + propertyInfo.Name;
-			PropertyInfo = propertyInfo;
-			return this;
-		}
-	}
+        public Type DataType => Tipo switch
+        {
+            TipoEnum.String => typeof(string),
+            TipoEnum.Int => typeof(int),
+            TipoEnum.Long => typeof(long),
+            TipoEnum.Double => typeof(double),
+            TipoEnum.Decimal => typeof(decimal),
+            TipoEnum.DateTime => typeof(DateTime),
+            TipoEnum.General => typeof(object),
+            _ => typeof(object),
+        };
+    }
 }
--- src/MPSTI.PlenoSoft.Core.Office.EPPlus/Controller/ExcelReport.cs
+++ src/MPSTI.PlenoSoft.Core.Office.EPPlus/Controller/ExcelReport.cs
@@ -9,57 +9,66 @@ using System.Threading.Tasks;
 
 namespace MPSTI.PlenoSoft.Core.Office.EPPlus.Controller
 {
-	public class ExcelReport
-	{
-		public async Task<byte[]> GerarAsync<T>(IEnumerable<T> list)
-		{
-			if (!list.Any()) return new byte[0];
+    public class ExcelReport
+    {
+        public async Task<byte[]> GerarAsync<T>(IEnumerable<T> list)
+        {
+            if (!list.Any()) return new byte[0];
 
-			using var stream = new MemoryStream();
-			using var package = new ExcelPackage(stream);
+            using var stream = new MemoryStream();
+            using var package = new ExcelPackage(stream);
 
-			package.Workbook.Properties.Author = "";
-			package.Workbook.Properties.Title = "";
-			var worksheet = package.Workbook.Worksheets.Add("Plan1");
+            package.Workbook.Properties.Author = "";
+            package.Workbook.Properties.Title = "";
+            var worksheet = package.Workbook.Worksheets.Add("Plan1");
 
-			var dataTable = GetDataTable(list);
-			worksheet.Cells.LoadFromDataTable(dataTable, true, OfficeOpenXml.Table.TableStyles.Light8);
+            var dataTable = GetDataTable(list);
+            worksheet.Cells.LoadFromDataTable(dataTable, true, OfficeOpenXml.Table.TableStyles.Light8);
 
-			package.Save();
+            package.Save();
 
-			stream.Position = 0;
+            stream.Position = 0;
 
-			return await Task.FromResult(stream.ToArray());
-		}
+            return await Task.FromResult(stream.ToArray());
+        }
 
-		private DataTable GetDataTable<T>(IEnumerable<T> list)
-		{
-			var dataTable = new DataTable();
+        private DataTable GetDataTable<T>(IEnumerable<T> list)
+        {
+            var dataTable = new DataTable();
 
-			var properties = list.FirstOrDefault()?.GetType().GetProperties();
-			var dataColumns = GetDataColumns(properties);
-			dataTable.Columns.AddRange(dataColumns.ToArray());
+            var properties = list.FirstOrDefault()?.GetType().GetProperties();
+            var attributes = GetAttributes(properties).OrderBy(x => x.Posicao);
+            var dataColumns = GetDataColumns(attributes);
+            dataTable.Columns.AddRange(dataColumns.ToArray());
 
-			foreach (var item in list)
-			{
-				var row = dataTable.NewRow();
-				row.ItemArray = properties.Select(x => x.GetValue(item)).ToArray();
-				dataTable.Rows.Add(row);
-			}
+            foreach (var item in list)
+            {
+                var row = dataTable.NewRow();
+                row.ItemArray = properties.Select(x => x.GetValue(item)).ToArray();
+                dataTable.Rows.Add(row);
+            }
 
-			return dataTable;
-		}
+            return dataTable;
+        }
 
-		private IEnumerable<DataColumn> GetDataColumns(PropertyInfo[] properties)
-		{
-			foreach (var property in properties)
-			{
-				foreach (var attribute in property.GetCustomAttributes(true))
-				{
-					if (attribute is ExcelColumnAttribute excelColumn)
-						yield return new DataColumn(excelColumn.Titulo);
-				}
-			}
-		}
-	}
+        private IEnumerable<ExcelColumnAttribute> GetAttributes(PropertyInfo[] properties)
+        {
+            foreach (var property in properties)
+            {
+                foreach (var attribute in property.GetCustomAttributes(true))
+                {
+                    if (attribute is ExcelColumnAttribute excelColumn)
+                        yield return excelColumn;
+                }
+            }
+        }
+
+        private IEnumerable<DataColumn> GetDataColumns(IEnumerable<ExcelColumnAttribute> attributes)
+        {
+            return attributes.Select(x => new DataColumn(x.PropertyName, x.DataType)
+            {
+                 Caption = x.Titulo,                  
+            });
+        }
+    }
 }
